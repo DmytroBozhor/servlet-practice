1bc3158402ade370c73d8f45b9618b350f292db4
DmytroBozhor <dimaastop1@gmail.com>
2024-03-08 22:42:58 +0100

    JSessionId

--- src/main/java/dmytro/bozhor/SessionServlet.java
+++ src/main/java/dmytro/bozhor/SessionServlet.java
@@ -2,19 +2,29 @@ package dmytro.bozhor;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 @WebServlet("/session")
 public class SessionServlet extends HttpServlet {
 
+    private static final String J_SESSION_ID = "JSESSIONID";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var session = req.getSession();
-        System.out.println(session.isNew());
+
+        if (!session.isNew()) {
+            System.out.println(J_SESSION_ID + ": " + Arrays.stream(req.getCookies())
+                    .filter(cookie -> J_SESSION_ID.equals(cookie.getName()))
+                    .findFirst().orElseThrow(RuntimeException::new).getValue());
+        }
+
     }
 }
